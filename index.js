0db8612e55dc71c7bf93a787cdceaffd7bb22d21
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-25 07:17:48 +0300

    modifies index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = 2000
+let b = 100
+let divide = (a, b) => a/b
+console.log (divide(a, b))
+let x = 2
+let square = (x) => x**2
+console.log (square(x))
+ a = 3
+ b = 4
+let add = (a, b) => a+b
+console.log (add(a, b))
